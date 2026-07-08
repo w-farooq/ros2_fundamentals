6615ee23cdd963a2d3dd69b07d47edab4c5c1c7f
Waqar Farooq <farooqwaqar18@gmail.com>
2026-07-08 22:18:46 +0100

    REFACTORED minimal c++ node to using oop

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -2,6 +2,30 @@
 
 
 
+class MyNode : public rclcpp::Node{
+
+    public:
+    MyNode () : Node("cpp_test"), counter_{}{
+        // Logging a message 
+        RCLCPP_INFO(this->get_logger(), "Hello World");
+
+        // Creating a timer 
+        // Create wall timer function takes two params first one is time, second is the callback function. 
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyNode::timer_call_back, this));
+
+    };
+
+    private:
+
+    void timer_call_back () {
+        RCLCPP_INFO(this->get_logger(), "Hello from the C++ timer callback method: %d", counter_);
+        ++counter_;
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+
+};
+
 
 int main (int argc, char **argv) {
 
@@ -9,16 +33,14 @@ int main (int argc, char **argv) {
     rclcpp::init(argc, argv);
 
 
-    // Creating a Node 
-    auto node {std::make_shared<rclcpp::Node>()};
+    // Creating a sharde pointer to an object of MyNode class
+    auto node {std::make_shared<MyNode>()};
 
-
-    // Logging a message 
-    RCLCPP_INFO(node->get_logger(), "Hello World");
 
     
-
-
+    // Spinning the node 
+    rclcpp::spin(node);
+    
 
     // Shutting down the node 
     rclcpp::shutdown();
