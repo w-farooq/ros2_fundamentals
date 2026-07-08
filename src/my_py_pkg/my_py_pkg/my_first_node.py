20e9a27aba4665ad29b148a525f14cf542ece7cb
Waqar Farooq <farooqwaqar18@gmail.com>
2026-07-08 18:06:59 +0100

    Refactor my_first_node to use OOP style

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -3,19 +3,29 @@
 import rclpy 
 from rclpy.node import Node
 
+
+class MyNode (Node) :
+    def __init__(self) :
+        super().__init__("py_test")
+        self.get_logger().info("Hello world")
+
+
+
+
+
 def main (args=None) :
     # This will initlalise the ros2 communication 
     rclpy.init(args=args)
 
-    # This will create a Node 
-    node = Node("py_test")
+    # This will create a Node with the name py_test 
+    node = MyNode()
 
-    # Adding logging 
-    node.get_logger().info("Hellow World")
+    # Keeping the node alive by passing it fo the sping function as an argument
+    rclpy.spin(node)
 
     # This will shutdown the node
     rclpy.shutdown()
 
 
-    if __name__ == "__main__" :
-        main()
+if __name__ == "__main__" :
+    main()
