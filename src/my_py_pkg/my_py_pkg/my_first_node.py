d670541592761d8f83ed5efea49526ee084cefc2
Waqar Farooq <farooqwaqar18@gmail.com>
2026-07-08 20:49:43 +0100

    Add timer and call-back functionality

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,7 +7,18 @@ from rclpy.node import Node
 class MyNode (Node) :
     def __init__(self) :
         super().__init__("py_test")
+        self.counter_ = 0
         self.get_logger().info("Hello world")
+
+        # create_timer function takes two arguments : 
+        # first float timer period in seconds 
+        # second callback_function 
+        self.create_timer(1.0, self.timer_callback)
+    
+    # Timer callback 
+    def timer_callback (self) : 
+        self.get_logger().info("Hello from the timer method count: " + str(self.counter_))
+        self.counter_ +=1
 
 
 
@@ -20,7 +31,8 @@ def main (args=None) :
     # This will create a Node with the name py_test 
     node = MyNode()
 
-    # Keeping the node alive by passing it fo the sping function as an argument
+    # Keeping the node alive by passing it to the spin function as an argument
+    # It will allow the node to process any callbacks 
     rclpy.spin(node)
 
     # This will shutdown the node
